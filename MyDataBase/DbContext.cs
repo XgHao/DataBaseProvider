d8d1d558db16b363606c71128e9bb9b97de5eec4
XgHao <zxh957553851@gamil.com>
2020-03-23 11:27:51 +0800

    DbHelper

---
+++ MyDataBase/DbContext.cs
@@ -0,0 +1,30 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDataBase
+{
+    /// <summary>
+    /// 数据库连接-配置信息
+    /// </summary>
+    public class DbContext
+    {
+        /// <summary>
+        /// 连接信息
+        /// </summary>
+        private static ConnectionConfig ConConfig = new ConnectionConfig
+        {
+            ConnectionString = ConfigurationManager.AppSettings["DbConfig"].ToString(),
+            DbType = DbType.SqlServer,
+            IsAutoCloseConnection = true,
+            InitKeyType = InitKeyType.Attribute
+        };
+
+        ///连接实例
+        public SqlSugarClient Db { get; } = new SqlSugarClient(ConConfig);
+    }
+}
---
+++ MyDataBase/DbEntities.cs
@@ -0,0 +1,14 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDataBase
+{
+    public class DbEntities<T> : DbContext where T : class, new()
+    {
+        public SimpleClient<T> GetSimpleClient() => new SimpleClient<T>(Db);
+    }
+}
